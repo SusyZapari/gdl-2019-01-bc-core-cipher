e36afde71fcd4a7c402a1f53109064a04b994fe8
susy zapari <susydev@99.200-57-1.bestel.com.mx>
2019-01-29 12:01:38 -0600

    cambio de titulo

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,16 @@
-window.cipher = {
-  // ... 
-};
+function encriptar() {
+  var res = String.fromCharCode(65);
+  var input = "GASTRITIS";
+  var offset = 3;
+  var resultado = "";
+
+  for (var indice = 0; indice < input.length; indice++) {
+     var codigoDeLetra = input.charCodeAt(indice); // convertir letra en numero
+     var nuevoCodigoDeLetra = codigoDeLetra + offset; // sumarle el offset (moverlo n posiciones)
+     var letraApartirDeCodigo = String.fromCharCode(nuevoCodigoDeLetra); // convertir el numero en letra
+
+    resultado += letraApartirDeCodigo;
+  }
+
+  document.getElementById("demo").innerHTML = resultado;
+}
